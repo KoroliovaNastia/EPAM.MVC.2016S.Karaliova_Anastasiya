037b0bea8a03d161e0f442fee93374ebb417c848
KoroliovaNastia <K.Nastia.san@mail.ru>
2016-08-31 11:26:59 +0300

    routing

---
+++ Day1/Routing/WebApplication/App_Start/RouteConfig.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Customer",
+                url: "{controller}/{action}/{document}/{*data}",
+                defaults: new { controller = "Home ", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                "SpecialRoute", "Special/{action}/{id}/{*catchall}",
+                new { controller = "Product", action = "OutputToJson", id = @"\d+" },
+                new[] { "MyCom.Team1" }
+                );
+
+            routes.MapRoute(
+                "GeneralRoute", "Product/{action}/{id: int}",
+                 new { controller = "Product", action = "Index" },
+                 new[] { "MyCom.Team2" }
+              );
+        }
+
+    }
+}
+
---
+++ Day1/Routing/WebApplication/Controllers/SpecialController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class SpecialController : Controller
+    {
+        // GET: Special
+        public JsonResult OutputToJson()
+        {
+
+            string[] model = new string[]
+        {
+            "Action",
+            "Json"
+        };
+            return Json(model, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
